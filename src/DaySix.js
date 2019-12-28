b9d4ce46b74b9c80c4d1b5632fafc2513271283f
Jeannette <jeannette@tastapod.com>
2019-12-28 08:08:31 +0000

    add solution for part two

--- src/DaySix.js
+++ src/DaySix.js
@@ -1,22 +1,43 @@
-const findOrbits = (object, mapData, orbits) => {
+const findOrbits = (object, mapData, path) => {
     if (!mapData.has(object)) {
-        return orbits
+        return path
     }
-    if (mapData.get(object) == 'COM') {
-        orbits++
-        return orbits
+    if (mapData.get(object) === 'COM') {
+        path.push(mapData.get(object))
+        return path
 }
-    orbits++
-    return findOrbits(mapData.get(object), mapData, orbits)
+    path.push(mapData.get(object))
+    return findOrbits(mapData.get(object), mapData, path)
+}
+
+const getPathYOUtoSAN = (pathOne, pathTwo) => {
+    let mergedPath = []
+    for (let item of pathOne) {
+        if (pathTwo.includes(item)) {
+            mergedPath.push(item)
+            break
+        } else {
+            mergedPath.push(item)
+        }
+        
+    };
+    const reversed = pathTwo.reverse()
+    reversed.forEach(item => {
+        if (!mergedPath.includes(item) && !pathOne.includes(item)) {
+            mergedPath.push(item)
+        }
+    });
+    return mergedPath
 }
 
 const getAllOrbits = (mapData, totalOrbits) => {
     for (let key of mapData.keys()) {
-        totalOrbits += findOrbits(key, mapData, 0)
+        totalOrbits += findOrbits(key, mapData, []).length
       }
     return totalOrbits
 }
 
 exports.findOrbits = findOrbits
 exports.getAllOrbits = getAllOrbits
+exports.getPathYOUtoSAN = getPathYOUtoSAN
 
--- test/DaySix.test.js
+++ test/DaySix.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { findOrbits, getAllOrbits } = require('../src/DaySix')
+const { findOrbits, getAllOrbits, getPathYOUtoSAN } = require('../src/DaySix')
 const fs = require('fs');
 
 describe('Test paths with sample input', () => {
@@ -8,13 +8,13 @@ describe('Test paths with sample input', () => {
         nodeMap.set('D', 'C')
         nodeMap.set('C', 'B')
         nodeMap.set('B', 'COM')
-        assert.equal(findOrbits('D', nodeMap, 0), 3)
+        assert.equal(findOrbits('D', nodeMap, []).length, 3)
     })
 
     it('prints number of orbits root to node for direct', () => {
         const nodeMap = new Map()
         nodeMap.set('B', 'COM')
-        assert.equal(findOrbits('B', nodeMap, 0), 1)
+        assert.equal(findOrbits('B', nodeMap, []).length, 1)
     })
 
     it('prints number of orbits root to node for all from L', () => {
@@ -30,7 +30,7 @@ describe('Test paths with sample input', () => {
         nodeMap.set('J', 'E')
         nodeMap.set('K', 'J')
         nodeMap.set('L', 'K')
-        assert.equal(findOrbits('L', nodeMap, 0), 7)
+        assert.equal(findOrbits('L', nodeMap, []).length, 7)
     })
 
     it('find total number of orbits for all nodes', () => {
@@ -49,6 +49,26 @@ describe('Test paths with sample input', () => {
         assert.equal(getAllOrbits(nodeMap, 0), 42)
     })
 
+    it('find path from YOU to SAN', () => {
+        const nodeMap = new Map()
+        nodeMap.set('B', 'COM')
+        nodeMap.set('C', 'B')
+        nodeMap.set('D', 'C')
+        nodeMap.set('E', 'D')
+        nodeMap.set('F', 'E')
+        nodeMap.set('G', 'B')
+        nodeMap.set('H', 'G')
+        nodeMap.set('I', 'D')
+        nodeMap.set('J', 'E')
+        nodeMap.set('K', 'J')
+        nodeMap.set('L', 'K')
+        nodeMap.set('YOU', 'K')
+        nodeMap.set('SAN', 'I')
+        const pathOne = findOrbits('YOU', nodeMap, [])
+        const pathTwo = findOrbits('SAN', nodeMap, [])
+        assert.deepEqual(getPathYOUtoSAN(pathOne, pathTwo), [ 'K', 'J', 'E', 'D', 'I' ])
+    })
+
     describe('Test orbit with actual input', () => {
         it('prints number of orbits root to node with real input', () => {
             
@@ -60,6 +80,20 @@ describe('Test paths with sample input', () => {
             }
             textByLine.forEach(createOrbitMap);
             console.log(getAllOrbits(mapData, 0))
+        })
+
+        it('prints number of orbits root to node with real input', () => {
+            
+            const mapData = new Map()
+            const textByLine = fs.readFileSync('orbits.txt').toString().split('\n');
+            const createOrbitMap = (orbit) => {
+                const split = orbit.split(')')
+                mapData.set(split[1], split[0])
+            }
+            textByLine.forEach(createOrbitMap);
+            const pathOne = findOrbits('YOU', mapData, [])
+            const pathTwo = findOrbits('SAN', mapData, [])
+            console.log(getPathYOUtoSAN(pathOne, pathTwo).length-1)
         })
     })
 })
